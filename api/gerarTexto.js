ab0f00e1935f76ed2e55bf21216c0cbf37efe80d
RogerioDP44 <115008024+RogerioDP44@users.noreply.github.com>
2026-02-20 14:39:46 -0300

    Add files via upload

--- api/gerarTexto.js
+++ api/gerarTexto.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
                 messages: [
                     {
                         role: "system",
-                        content: "Você é um especialista em SEO Shopee Brasil. Responda seguindo RIGOROSAMENTE este formato: TITULO ### DESCRIÇÃO ### TAGS. Regras: 1. O TITULO deve ser apenas texto, SEM EMOJIS. 2. A DESCRIÇÃO deve ter muitos emojis. 3. As TAGS devem ter #. Use apenas ### como separador."
+                        content: "Você é um especialista em SEO Shopee Brasil. Responda seguindo RIGOROSAMENTE este formato: TITULO ### DESCRIÇÃO ### TAGS. Regras: 1. O TITULO deve ser apenas texto entre 70 e 90 caracteres. Nem mais nem menos, SEM EMOJIS Foque em palavras-chaves de alta conversão. 2. A DESCRIÇÃO deve ter muitos emojis. 3. As TAGS devem ter #. Use apenas ### como separador."
                     },
                     { role: "user", content: `Produto: ${nome}, Preço: ${preco}` }
                 ],
